7e4660783e837c5b74487721f0a72ae2d5bfa16a
shaibula-mehdi <shaibulamehdi@gmail.com>
2017-03-23 13:57:44 +0600

    Update switch-tools.py

--- switch-tools.py
+++ switch-tools.py
@@ -3,11 +3,6 @@ import re
 import telnetlib
 from socket import error as socket_error
 
-class SwitchTolls():
-  """docs, add later"""
-  def __init__(self, arg)
-    pass
-
 def get_vendor(host):
   """later, need net-snmp lib, www.net-snmp.org"""
   pass
@@ -15,23 +10,8 @@ def get_vendor(host):
 def bulk(self, cmd, opt):
   pass
 
-class DLink():
-  """docs, add later"""
-  def __init(self):
-    self.session = telnetlib.Telnet()
-    self.session.set_option_negotiation(bulk) #for fast opertaion, this disable func WILL, DO, WONT etc
-    #self.session.set_debuglevel(0)
-    self.connect_state = False
-    self.auth_state = False
-  
-  def connect():
-    pass
-  
-  def auth():
-    pass
-  
-  def disconnect():
-    pass
-  
-  def send():
-    pass
+def selector():
+  pass
+
+def sender():
+  pass
