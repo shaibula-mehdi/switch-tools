04a63680fc34517ddda6f1bd47d3e284c3ec2168
shaibula-mehdi <shaibulamehdi@gmail.com>
2017-03-23 14:36:04 +0600

    Update switch-tools.py

--- switch-tools.py
+++ switch-tools.py
@@ -3,6 +3,15 @@ import re
 import telnetlib
 from socket import error as socket_error
 
+DLINK_DES3526 = []
+DLINK_C1 = []
+DLINK_3010 = []
+DLINK_A1B1 = []
+DLINK_OTHER = []
+
+CISCO_ME3XXX = []
+EDGECORE = []
+
 def get_vendor(host):
   """later, need net-snmp lib, www.net-snmp.org"""
   pass
@@ -10,8 +19,37 @@ def get_vendor(host):
 def bulk(self, cmd, opt):
   pass
 
-def selector():
+def selector(host, model):
+  if model in DLINK_DES3526:
+    pass
+  elif model in DLINK_C1:
+    pass
+  elif model in DLINK_3010:
+    pass
+  elif model in DLINK_A1B1:
+    pass
+  elif model in DLINK_OTHER:
+    pass
+  elif model in CISCO_ME3XXX:
+    pass
+  elif model in EDGECORE:
+    pass
+
+
+def connection(host, opt_negotiation = False):
+  try:
+    session = telnetlib.Telnet()
+    if opt_negotiation == True:
+      session.set_option_negotiation(bulk)
+  except socket_error as e:
+    print(e)
+    
+
+def auth(connection):
+  pass
+  
+  
+def sender(connection):
   pass
 
-def sender():
-  pass
+
